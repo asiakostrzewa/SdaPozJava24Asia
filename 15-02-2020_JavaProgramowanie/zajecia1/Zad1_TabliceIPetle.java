b828168e3707cd3d4b8b6b12dae960cb926d9181
asiakostrzewa <joanna.o.kostrzewa@tlen.pl>
2020-02-15 12:28:08 +0100

    - added files

---
+++ 15-02-2020_JavaProgramowanie/zajecia1/Zad1_TabliceIPetle.java
@@ -0,0 +1,39 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Zad1_TabliceIPetle {
+    public static void main(String[] args) {
+        System.out.println("Podaj 6 cyfr z zakresu 1 - 49. Liczba 1:");
+        Scanner scanner = new Scanner(System.in);
+        int li1 = scanner.nextInt();
+        System.out.println("liczba 2: ");
+        int li2 = scanner.nextInt();
+        System.out.println("liczba 3: ");
+        int li3 = scanner.nextInt();
+        System.out.println("liczba 4: ");
+        int li4 = scanner.nextInt();
+        System.out.println("liczba 5: ");
+        int li5 = scanner.nextInt();
+        System.out.println("liczba 6: ");
+        int li6 = scanner.nextInt();
+
+        System.out.println("Wytypowane przez Ciebie liczby to: "+li1+", "+li2+", "+li3+", "+li4+", "+li5+", "+li6);
+
+        Random r = new Random();
+        System.out.print("Wylosowane liczby to: Liczba 1: "+ r[1] = r.nextInt(49)+1);
+        System.out.print(", liczba 2: "+ r[2] = r.nextInt(49)+1);
+        System.out.print(", liczba 3: "+ r[3] = r.nextInt(49)+1);
+        System.out.print(", liczba 4: "+ r[4] = r.nextInt(49)+1);
+        System.out.print(", liczba 5: "+ r[5] = r.nextInt(49)+1);
+        System.out.print(", liczba 6: "+ r[6] = r.nextInt(49)+1);
+
+        int suma = 0;
+        for (int i = 0; i <li.length; i++){
+            for (int j = 0; j < r.length; j++){
+                suma++;
+            }
+        }
+
+        System.out.println("Trafiono "+ suma + " z 6 cyfr");
+    }
+}
